8bdefbbc97765648f8a878ccfc3fcf9712931113
hzx <2663831724@QQ.com>
2024-11-07 22:05:58 +0800

    1.添加按钮组。

---
+++ UllnnovationHub.ToolKIt/Controls/Attached/Corner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UllnnovationHub.ToolKIt.Controls.Attached
+{
+	/// <summary>
+	/// 附加属性，用于控件的圆角
+	/// </summary>
+	public class Corner
+	{
+		#region 圆角值
+		public static CornerRadius GetValue(DependencyObject obj)
+		{
+			return (CornerRadius)obj.GetValue(ValueProperty);
+		}
+
+		public static void SetValue(DependencyObject obj, CornerRadius value)
+		{
+			obj.SetValue(ValueProperty, value);
+		}
+
+		public static readonly DependencyProperty ValueProperty =
+			DependencyProperty.RegisterAttached("Value", typeof(CornerRadius), typeof(Corner), new PropertyMetadata(default));
+		#endregion
+	}
+}
