300e642341cd8ddaf39f04768428a38767a503ad
hzx <2663831724@QQ.com>
2024-11-11 17:47:35 +0800

    1.添加分离横线。

---
+++ UllnnovationHub.ToolKIt/Themes/Separator/VerticalSeparator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UllnnovationHub.ToolKIt.Themes.Separator
+{
+	public class VerticalSeparator : Control
+	{
+		static VerticalSeparator()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(VerticalSeparator), new FrameworkPropertyMetadata(typeof(VerticalSeparator)));
+		}
+	}
+}
